dc26e11014015202bd068dff0f233fe579a4f3d1
Chase <chasehillig@yahoo.com>
2022-07-12 12:31:40 -0600

    added css baseline styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import React from "react"
+import Container from "@mui/material/Container"
+import CssBaseline from "@mui/material/CssBaseline"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
 
 const Home = () => {
+  const theme = createTheme()
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container component='main' maxWidth='lg'>
+        <CssBaseline />
+        <div>
+          <h1>Home</h1>
+        </div>
+      </Container>
+    </ThemeProvider>
   )
 }
 
